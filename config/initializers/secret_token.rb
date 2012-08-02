cbcec08c23605d7128e8095f056312ac5547db85
Brad Griffith <braddgriffith@gmail.com>
2012-08-02 14:26:00 -0700

    Initial commit - built the List functionality.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoList::Application.config.secret_token = 'd655e7a3289598037462901d9370ed47001256d85251d09899a13e137b1520e98d4b1750ce1593cb6449bf8d9f3a44f96d980ce557398a5e26cfe6b9f53f0f1a'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+ToDoList::Application.routes.draw do
+  resources :lists
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+end
